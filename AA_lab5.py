15b57138f59bf199cdf10d31208011921b1ac66f
TomaszJankowiak <62778369+TomaszJankowiak@users.noreply.github.com>
2022-06-03 15:39:33 +0200

    first commit

---
+++ AA_lab5.py
@@ -0,0 +1,23 @@
+
+
+def MutualExclusion(n):
+    P=[]
+    SK=[]
+    for i in range(0,n):
+        P.append(-1)
+        SK.append(-1)
+    step=0
+
+
+
+    return step
+
+n = 5
+
+
+
+
+#Maksymalna liczba krokow do konfiguracji legalnej
+print('n =',n,'\nworst case:',MutualExclusion(n))
+
+
