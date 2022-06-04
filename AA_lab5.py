da5ff1f7c84e44b51c8e720a14f7f02ae2bbfffd
TomaszJankowiak <62778369+TomaszJankowiak@users.noreply.github.com>
2022-06-04 19:17:58 +0200

    refactoring, balagan i brak pomyslu, ale za to nie dziala :(

--- AA_lab5.py
+++ AA_lab5.py
@@ -1,19 +1,10 @@
 
 
-def iflegal(SK):
-    if sum(SK) == 1:
-        konfiguracja_legalna = True
-    else:
-        konfiguracja_legalna = False
-    return konfiguracja_legalna
-
-def MutualExclusion(n,P):
-    SK=[] # sekcja krytyczna
+def iflegal(P):
+    SK=[] # sekcja krytyczna (mozliwosc ruchu dla kazdego wezla)
+    n = len(P)
     for i in range(0,n):
-        P.append(-1)
         SK.append(2)
-    step=0
-
     if P[0] == P[n-1]:
         SK[0] = 1
     else:
@@ -23,24 +14,44 @@ def MutualExclusion(n,P):
             SK[i] = 1
         else:
             SK[i] = 0
+    if sum(SK) == 1:
+        konfiguracja_legalna = True
+    else:
+        konfiguracja_legalna = False
+    return konfiguracja_legalna
 
 
-        
+def MutualExclusion(n,P,step):
+    worst_case = 0
+    if step > worst_case:
+        worst_case = step
+
+    if iflegal(P) == True:
+        return worst_case
     
+    for i in range(1,n):
+        if P[i] != P[i-1]:
+            A = P
+            k = step
+            k += 1
+            MutualExclusion(n,A,k)
     
-    return step
+  
+    return worst_case
 
-n = 5
+
+n = 3
 P=[] # tokeny
-
 for i in range(0,n):
-    for j in range(0,n):
-        for k in range(0,n):
-            i=n
+    P.append(-1)
+
+P[0]=0
+P[1]=1
+P[2]=1
 
 
 
 #Maksymalna liczba krokow do konfiguracji legalnej
-print('n =',n,'\nworst case:',MutualExclusion(n,P))
+print('n =',n,'\nworst case:',MutualExclusion(n,P,0))
 
 
