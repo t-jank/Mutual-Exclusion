18cb5b038047c32a00a23f2373e6a86f37bc0638
TomaszJankowiak <62778369+TomaszJankowiak@users.noreply.github.com>
2022-06-13 10:19:26 +0200

    algorytm fixed, jeszcze licznik

--- MutualExclusion.py
+++ MutualExclusion.py
@@ -19,16 +19,16 @@ def iflegal(P):
     return konfiguracja_legalna
 
 def move(P,x):
+    A=P
     if x==0:
-        P[x] = (P[x]+1) % (len(P)+1)
+        A[x] = (A[x]+1) % (len(A)+1)
     else:
-        P[x] = P[x-1]
-    return P
+        A[x] = A[x-1]
+    return A
 
-def MutualExclusion(n,P,step):
+def MutualExclusion(n,P):
     worst_case = 0
-   # if step > worst_case:
-  #      worst_case = step
+    
     SK=[]
     for i in range(0,n):
         SK.append(2)
@@ -42,63 +42,63 @@ def MutualExclusion(n,P,step):
         else:
             SK[i] = 0
     if iflegal(P) == True:
-        return step
-    
-    for i in range(0,n):
-        if SK[i]==1:
+        return worst_case
+
+    for s in range(0,n):
+        if SK[s]==1:
             A = P
-            k = step
-            k += 1
-            worst_case=max(worst_case,MutualExclusion(n,move(A,i),step+1))
-    
+            powrot_guarantee = P[s]
+           # worst_case=max(worst_case,MutualExclusion(n,move(A,i)))
+            MutualExclusion(n,move(A,s))
+            P[s] = powrot_guarantee
     return worst_case
 
 
 def all_configurations(n):
     worst = 0
     if n==2:
-        for a in range(0,n):
-            for b in range(0,n):
+        for a in range(0,n+1):
+            for b in range(0,n+1):
                 P=[a,b]
-                z = MutualExclusion(n,P,0)
+                z = MutualExclusion(n,P)
                 if worst < z:
                     worst = z
     if n==3:
-        for a in range(0,n):
-            for b in range(0,n):
-                for c in range(0,n):
+        for a in range(0,n+1):
+            for b in range(0,n+1):
+                for c in range(0,n+1):
                     P=[a,b,c]
-                    z = MutualExclusion(n,P,0)
+                    z = MutualExclusion(n,P)
                     if worst < z:
                         worst = z
     if n==4:
-        for a in range(0,n):
-            for b in range(0,n):
-                for c in range(0,n):
-                    for d in range(0,n):
+        for a in range(0,n+1):
+            for b in range(0,n+1):
+                for c in range(0,n+1):
+                    for d in range(0,n+1):
                         P=[a,b,c,d]
-                        z = MutualExclusion(n,P,0)
+                        z = MutualExclusion(n,P)
                         if worst < z:
                             worst = z
     if n==5:
-        for a in range(0,n):
-            for b in range(0,n):
-                for c in range(0,n):
-                    for d in range(0,n):
-                        for e in range(0,n):
+        for a in range(0,n+1):
+            for b in range(0,n+1):
+                for c in range(0,n+1):
+                    for d in range(0,n+1):
+                        for e in range(0,n+1):
                             P=[a,b,c,d,e]
-                            z = MutualExclusion(n,P,0)
+                            z = MutualExclusion(n,P)
                             if worst < z:
                                 worst = z
     if n==6:
-        for a in range(0,n):
-            for b in range(0,n):
-                for c in range(0,n):
-                    for d in range(0,n):
-                        for e in range(0,n):
-                            for f in range(0,n):
+        for a in range(0,n+1):
+            for b in range(0,n+1):
+                for c in range(0,n+1):
+                    for d in range(0,n+1):
+                        for e in range(0,n+1):
+                            for f in range(0,n+1):
                                 P=[a,b,c,d,e,f]
-                                z = MutualExclusion(n,P,0)
+                                z = MutualExclusion(n,P)
                                 if worst < z:
                                     worst = z
     return worst
@@ -107,6 +107,6 @@ def all_configurations(n):
 n = 4
 
 #Maksymalna liczba krokow do konfiguracji legalnej
-print('n =',n,'\nworst case:',all_configurations(n))
+#print('n =',n,'\nworst case:',all_configurations(n))
 
-
+MutualExclusion(n,[1,1,2,1])
